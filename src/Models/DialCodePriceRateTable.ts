2b14b6964ce4248bb5ff8dd8de5c404b40d8f51c
Henrique Inonhe <henriqueinonhe@gmail.com>
2020-09-09 17:08:08 -0300

    Implemented and tested DialCodePriceRateTable

---
+++ src/Models/DialCodePriceRateTable.ts
@@ -0,0 +1,164 @@
+import Dinero from "dinero.js";
+
+type DialCodePriceRateTableData = Map<number, Map<number, Dinero.Dinero>>;
+
+/**
+ * Represents a table that stores the price rates
+ * of phone calls from a given dial code to another.
+ * 
+ * Class Invariants:
+ * - This class is immutable, so once created it 
+ * remains with the same state throughout its entire life.
+ * - If a given dial code is valid (that is, there is an entry associated with
+ * it in the table), then it has a well defined rate for phone calls
+ * performed from this dial code to every other valid code, that is, 
+ * the table behaves like a matrix where valid dial codes are the rows/columns.
+ * Simply put, for every dial code present as a row, it must also be present as a column.
+ * - Conversely, if a there is a rate defined for a given dial code pair, then 
+ * the "arrival" dial code must be a valid code and present as a row in the table.
+ * Simply put, for every dial code present as a column, it must also be present as a row.
+ */
+export default class DialCodePriceRateTable
+{
+  /**
+   * Computes valid dial codes, that is, any dial
+   * code that is either present as a row or as a column.
+   * 
+   * Pre Conditions:
+   * None
+   * 
+   * @param tableData 
+   */
+  private static computeValidDialCodes(tableData : DialCodePriceRateTableData) : Set<number>
+  {
+    const validDialCodes = new Set(tableData.keys()); //Valid dial codes extracted from table rows
+    for(const row of tableData.values())
+    {
+      for(const columnDialCode of row.keys())
+      {
+        validDialCodes.add(columnDialCode);
+      }
+    }
+    
+    return validDialCodes;
+  }
+
+  /**
+   * Checks whether [[DialCodePriceTableData]] is valid, that is,
+   * every dial code that is present as a row is also present as columns
+   * in each row and every dial code that is present as a column is also present
+   * as a row.
+   * 
+   * In the case validation fails, this function throws an exception detailing
+   * which dial codes are missing either as rows or as columns.
+   * 
+   * Pre Conditions:
+   * None
+   * 
+   * @param tableData 
+   */
+  private static validateDialCodePriceRateTableData(tableData : DialCodePriceRateTableData) : void
+  {
+    const validDialCodes = DialCodePriceRateTable.computeValidDialCodes(tableData);
+    
+    const missingDialCodesAsRows = [];
+    const dialCodesAsRows = new Set(tableData.keys());
+    for(const validDialCode of validDialCodes)
+    {
+      if(!dialCodesAsRows.has(validDialCode))
+      {
+        missingDialCodesAsRows.push(validDialCode);
+      }
+    }
+
+    if(missingDialCodesAsRows.length !== 0)
+    {
+      throw new Error(`Dial codes "${missingDialCodesAsRows.join(`", "`)}" are valid, but are missing as rows!`);
+    }
+
+    const missingDialCodesAsColumns : Array<{fromDialCode : number; missingDialCodes : Array<number>}> = [];
+    for(const fromDialCode of tableData.keys())
+    {
+      const dialCodeRow = tableData.get(fromDialCode)!;
+      const missingDialCodesEntry = {fromDialCode: fromDialCode, missingDialCodes: [] as Array<number>};
+      for(const validDialCode of validDialCodes)
+      {
+        if(!dialCodeRow.has(validDialCode))
+        {
+          missingDialCodesEntry.missingDialCodes.push(validDialCode);
+        }
+      }
+
+      if(missingDialCodesEntry.missingDialCodes.length !== 0)
+      {
+        missingDialCodesAsColumns.push(missingDialCodesEntry);
+      }
+    }
+
+    if(missingDialCodesAsColumns.length !== 0)
+    {
+      let missingDialCodesErrorMessage = "";
+      for(const missingDialCodeEntry of missingDialCodesAsColumns)
+      {
+        const {fromDialCode, missingDialCodes} = missingDialCodeEntry;
+        missingDialCodesErrorMessage += `\nDial codes "${missingDialCodes.join(`", "`)}" are missing from dial code row "${fromDialCode}"`;
+      }
+
+      throw new Error(`There are valid dial codes that are missing as columns!` + 
+                      `${missingDialCodesErrorMessage}`);
+    }
+  }
+  
+  /**
+   * Constructs a [[DialCodePriceRateTable]] instance.
+   * 
+   * Pre Conditions:
+   * - `tableData` must be valid. See [[DialCodePriceRateTable]] invariants.
+   * 
+   * @param tableData 
+   */
+  constructor(tableData : DialCodePriceRateTableData)
+  {
+    DialCodePriceRateTable.validateDialCodePriceRateTableData(tableData);
+    this.data = tableData;
+  }
+
+  /**
+   * Checks whether a given dial code is valid in this table instance.
+   * 
+   * Throws exception if validation fails.
+   * 
+   * Pre Conditions:
+   * None
+   * 
+   * @param dialCode 
+   */
+  private validateDialCode(dialCode : number) : void
+  {
+    if(!this.data.has(dialCode))
+    {
+      throw new Error(`"${dialCode}" is not a valid dial code!`);
+    }
+  }
+
+  /**
+   * Returns price rate associated with the given dial code pair if 
+   * it exists and throws exception otherwise.
+   * 
+   * Price rate is returned by value.
+   * 
+   * @param fromDialCode 
+   * @param toDialCode 
+   */
+  public getRate(fromDialCode : number, toDialCode : number) : Dinero.Dinero
+  {
+    this.validateDialCode(fromDialCode);
+    this.validateDialCode(toDialCode);
+
+    const rate = this.data.get(fromDialCode)!.get(toDialCode)!;
+    const rateClone = Dinero({amount: rate.getAmount(), currency: rate.getCurrency(), precision: rate.getPrecision()});
+    return rateClone;
+  }  
+
+  private data : DialCodePriceRateTableData;
+}
---
+++ tests/Models/DialCodePriceRateTable.test.ts
@@ -0,0 +1,188 @@
+import DialCodePriceRateTable from "../../src/Models/DialCodePriceRateTable";
+import Dinero from "dinero.js";
+
+const DineroReal = (amount : number) : Dinero.Dinero => Dinero({amount, currency: "BRL", precision: 4});
+
+describe("constructor", () =>
+{
+  describe("Pre Conditions", () =>
+  {
+    describe("Every valid code must be present as a row", () =>
+    {
+      test("Single dial code missing", () =>
+      {
+        const tableData = new Map<number, Map<number, Dinero.Dinero>>();
+  
+        tableData.set(1, new Map<number, Dinero.Dinero>());
+        tableData.get(1)!.set(1, DineroReal(0));
+        tableData.get(1)!.set(2, DineroReal(130));
+  
+        expect(() => new DialCodePriceRateTable(tableData)).toThrow(`Dial codes "2" are valid, but are missing as rows!`);
+
+        //Adding missing dial codes
+        tableData.set(2, new Map<number, Dinero.Dinero>());
+        tableData.get(2)!.set(1, DineroReal(130));
+        tableData.get(2)!.set(2, DineroReal(0));
+
+        expect(() => new DialCodePriceRateTable(tableData)).not.toThrow();
+      });
+
+      test("Multiple dial codes missing from different rows", () =>
+      {
+        const tableData = new Map<number, Map<number, Dinero.Dinero>>();
+
+        tableData.set(1, new Map<number, Dinero.Dinero>());
+        tableData.get(1)!.set(1, DineroReal(0));
+        tableData.get(1)!.set(2, DineroReal(2));
+        tableData.get(1)!.set(3, DineroReal(1));
+
+        tableData.set(2, new Map<number, Dinero.Dinero>());
+        tableData.get(2)!.set(1, DineroReal(2));
+        tableData.get(2)!.set(2, DineroReal(0));
+        tableData.get(2)!.set(3, DineroReal(1));
+        tableData.get(2)!.set(4, DineroReal(1));
+        tableData.get(2)!.set(5, DineroReal(3));
+
+        expect(() => new DialCodePriceRateTable(tableData)).toThrow(`Dial codes "3", "4", "5" are valid, but are missing as rows!`);
+        
+        //Adding missing dial codes
+        tableData.get(1)!.set(4, DineroReal(1));
+        tableData.get(1)!.set(5, DineroReal(3));
+
+        tableData.set(3, new Map<number, Dinero.Dinero>());
+        tableData.get(3)!.set(1, DineroReal(2));
+        tableData.get(3)!.set(2, DineroReal(0));
+        tableData.get(3)!.set(3, DineroReal(1));
+        tableData.get(3)!.set(4, DineroReal(1));
+        tableData.get(3)!.set(5, DineroReal(3));
+        
+        tableData.set(4, new Map<number, Dinero.Dinero>());
+        tableData.get(4)!.set(1, DineroReal(2));
+        tableData.get(4)!.set(2, DineroReal(0));
+        tableData.get(4)!.set(3, DineroReal(1));
+        tableData.get(4)!.set(4, DineroReal(1));
+        tableData.get(4)!.set(5, DineroReal(3));
+        
+        tableData.set(5, new Map<number, Dinero.Dinero>());
+        tableData.get(5)!.set(1, DineroReal(2));
+        tableData.get(5)!.set(2, DineroReal(0));
+        tableData.get(5)!.set(3, DineroReal(1));
+        tableData.get(5)!.set(4, DineroReal(1));
+        tableData.get(5)!.set(5, DineroReal(3));
+
+        expect(() => new DialCodePriceRateTable(tableData)).not.toThrow();
+      });
+    });
+
+    describe("Every valid code must be present as a column for every row", () =>
+    {
+      test("", () =>
+      {
+        const tableData = new Map<number, Map<number, Dinero.Dinero>>();
+
+        tableData.set(1, new Map<number, Dinero.Dinero>());
+        tableData.get(1)!.set(1, DineroReal(1));
+
+        tableData.set(2, new Map<number, Dinero.Dinero>());
+        tableData.get(2)!.set(2, DineroReal(1));
+        
+        tableData.set(3, new Map<number, Dinero.Dinero>());
+        tableData.get(3)!.set(3, DineroReal(1));
+
+        tableData.set(4, new Map<number, Dinero.Dinero>());
+        tableData.get(4)!.set(4, DineroReal(1));
+
+        tableData.set(5, new Map<number, Dinero.Dinero>());
+        tableData.get(5)!.set(5, DineroReal(1));
+
+        expect(() => new DialCodePriceRateTable(tableData)).toThrow(`There are valid dial codes that are missing as columns!\nDial codes "2", "3", "4", "5" are missing from dial code row "1"\nDial codes "1", "3", "4", "5" are missing from dial code row "2"\nDial codes "1", "2", "4", "5" are missing from dial code row "3"\nDial codes "1", "2", "3", "5" are missing from dial code row "4"\nDial codes "1", "2", "3", "4" are missing from dial code row "5"`);
+
+        //Adding missing dial codes
+        tableData.get(1)!.set(2, DineroReal(0));
+        tableData.get(1)!.set(3, DineroReal(1));
+        tableData.get(1)!.set(4, DineroReal(1));
+        tableData.get(1)!.set(5, DineroReal(3));
+
+        tableData.get(2)!.set(1, DineroReal(0));
+        tableData.get(2)!.set(3, DineroReal(1));
+        tableData.get(2)!.set(4, DineroReal(1));
+        tableData.get(2)!.set(5, DineroReal(3));
+
+        tableData.get(3)!.set(1, DineroReal(0));
+        tableData.get(3)!.set(2, DineroReal(1));
+        tableData.get(3)!.set(4, DineroReal(1));
+        tableData.get(3)!.set(5, DineroReal(3));
+
+        tableData.get(4)!.set(1, DineroReal(0));
+        tableData.get(4)!.set(2, DineroReal(1));
+        tableData.get(4)!.set(3, DineroReal(1));
+        tableData.get(4)!.set(5, DineroReal(3));
+
+        tableData.get(5)!.set(1, DineroReal(0));
+        tableData.get(5)!.set(2, DineroReal(1));
+        tableData.get(5)!.set(3, DineroReal(1));
+        tableData.get(5)!.set(4, DineroReal(3));
+
+        expect(() => new DialCodePriceRateTable(tableData)).not.toThrow();
+      });
+    });
+
+  });
+});
+
+describe("getRate()", () =>
+{
+  const tableData = new Map<number, Map<number, Dinero.Dinero>>();
+
+  tableData.set(11, new Map<number, Dinero.Dinero>());
+  tableData.get(11)!.set(11, DineroReal(0));
+  tableData.get(11)!.set(16, DineroReal(190e2));
+  tableData.get(11)!.set(17, DineroReal(170e2));
+  tableData.get(11)!.set(18, DineroReal(90e2));
+
+  tableData.set(16, new Map<number, Dinero.Dinero>());
+  tableData.get(16)!.set(11, DineroReal(290e2));
+  tableData.get(16)!.set(16, DineroReal(0));
+  tableData.get(16)!.set(17, DineroReal(0));
+  tableData.get(16)!.set(18, DineroReal(0));
+
+  tableData.set(17, new Map<number, Dinero.Dinero>());
+  tableData.get(17)!.set(11, DineroReal(270e2));
+  tableData.get(17)!.set(16, DineroReal(0));
+  tableData.get(17)!.set(17, DineroReal(0));
+  tableData.get(17)!.set(18, DineroReal(0));
+
+  tableData.set(18, new Map<number, Dinero.Dinero>());
+  tableData.get(18)!.set(11, DineroReal(190e2));
+  tableData.get(18)!.set(16, DineroReal(0));
+  tableData.get(18)!.set(17, DineroReal(0));
+  tableData.get(18)!.set(18, DineroReal(0));
+
+  const table = new DialCodePriceRateTable(tableData);
+
+  describe("Pre Conditions", () =>
+  {
+    test("FromDialCode must be valid", () =>
+    {
+      expect(() => table.getRate(10, 11)).toThrow(`"10" is not a valid dial code!`);
+    });
+
+    test("ToDialCode must be valid", () =>
+    {
+      expect(() => table.getRate(11, 20)).toThrow(`"20" is not a valid dial code!`);
+    });
+  });
+
+  describe("Post Conditions", () =>
+  {
+    test("Happy path", () =>
+    {
+      expect(table.getRate(11, 16).equalsTo(DineroReal(190e2))).toBe(true);
+      expect(table.getRate(16, 11).equalsTo(DineroReal(290e2))).toBe(true);
+      expect(table.getRate(11, 17).equalsTo(DineroReal(170e2))).toBe(true);
+      expect(table.getRate(17, 11).equalsTo(DineroReal(270e2))).toBe(true);
+      expect(table.getRate(11, 18).equalsTo(DineroReal(90e2))).toBe(true);
+      expect(table.getRate(18, 11).equalsTo(DineroReal(190e2))).toBe(true);
+    });
+  });
+});
