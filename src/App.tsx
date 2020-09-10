99718689a014345e35a2915a301a0831cd67caef
Henrique Inonhe <henriqueinonhe@gmail.com>
2020-09-10 18:40:40 -0300

    Implemented UI first version for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { FaleMaisPlan } from "./Models/DialPriceCalculator";
 import { DineroReal } from "./Utils/Utils";
 import DialCodeCalculator from "./Models/DialPriceCalculator";
 import DialCodePriceRateTable from "./Models/DialCodePriceRateTable";
+import { Main, PriceCalculator, PriceCalculatorTitle, FieldContainer, InputLabel, NumberInput, FaleMaisSelect, FaleMaisOption, CalculateButton, DisplayContainer, DisplayLabel, PriceDisplay } from "./AppSubComponents";
 
 //Temp Table
 const rateTableData = new Map<number, Map<number, Dinero.Dinero | null>>();
 
 rateTableData.set(11, new Map<number, Dinero.Dinero | null>());
-rateTableData.get(11)!.set(11, DineroReal(0));
+rateTableData.get(11)!.set(11, null);
 rateTableData.get(11)!.set(16, DineroReal(190e2));
 rateTableData.get(11)!.set(17, DineroReal(170e2));
 rateTableData.get(11)!.set(18, DineroReal(90e2));
@@ -34,11 +35,10 @@ rateTableData.get(18)!.set(18, null);
 
 const rateTable = new DialCodePriceRateTable(rateTableData);
 
-
 function App() : JSX.Element
 {
   const [fromDialCode, setFromDialCode] = useState<number>(11);
-  const [toDialCode, setToDialCode] = useState<number>(11);
+  const [toDialCode, setToDialCode] = useState<number>(16);
   const [dialLengthInMinutes, setDialLengthInMinutes] = useState<number>(0);
   const [faleMaisPlan, setFaleMaisPlan] = useState<FaleMaisPlan>(FaleMaisPlan.FaleMais30);
   const [defaultPrice, setDefaultPrice] = useState<Dinero.Dinero | null>(null);
@@ -65,33 +65,70 @@ function App() : JSX.Element
     }
   }
 
-  return (
-    <>
-      <input 
-        type="number" 
-        value={fromDialCode}
-        onChange={(event) => {setFromDialCode(parseInt(event.target.value));}}
-      />
-      <input 
-        type="number" 
-        value={toDialCode}
-        onChange={(event) => {setToDialCode(parseInt(event.target.value));}}
-      />
-      <input 
-        type="number" 
-        value={dialLengthInMinutes}
-        onChange={(event) => {setDialLengthInMinutes(parseInt(event.target.value));}}
-      />
-      <select value={faleMaisPlan} onChange={(event) => {setFaleMaisPlan(event.target.value as FaleMaisPlan);}}>
-        <option value="FaleMais30">FaleMais30</option>
-        <option value="FaleMais60">FaleMais60</option>
-        <option value="FaleMais120">FaleMais120</option>
-      </select>
+  useEffect(() =>
+  {
+    handleCalculate();
+  }, [fromDialCode, toDialCode, dialLengthInMinutes, faleMaisPlan]);
 
-      <input type="text" readOnly value={defaultPrice?.setLocale("pt-BR").toFormat() ?? "Não Disponível"}/>
-      <input type="text" readOnly value={faleMaisPrice?.setLocale("pt-BR").toFormat() ?? "Não Disponível"}/>
-      <button onClick={handleCalculate}>Calcular</button>
-    </>
+  return (
+    <Main>
+      <h1>Telzir</h1>
+
+      <PriceCalculator>
+        <PriceCalculatorTitle>Compare os Preços!</PriceCalculatorTitle>
+
+        <FieldContainer>
+          <InputLabel htmlFor="fromDialCodeInput">DDD de Origem</InputLabel>
+          <NumberInput 
+            value={fromDialCode.toString()}
+            onChange={(event) => {setFromDialCode(parseInt(event.target.value));}}
+            id="fromDialCodeInput"
+          />
+        </FieldContainer>
+        
+        <FieldContainer>
+          <InputLabel htmlFor="toDialCodeInput">DDD de Destino</InputLabel>
+          <NumberInput 
+            value={toDialCode.toString()}
+            onChange={(event) => {setToDialCode(parseInt(event.target.value));}}
+            id="toDialCodeInput"
+          />
+        </FieldContainer>
+
+        <FieldContainer>
+          <InputLabel htmlFor="dialLengthInMinutesInput">Duração da Ligação (Minutos)</InputLabel>
+          <NumberInput 
+            value={dialLengthInMinutes.toString()}
+            onChange={(event) => {setDialLengthInMinutes(parseInt(event.target.value));}}
+            id="dialLengthInMinutesInput"
+          />
+        </FieldContainer>
+
+        <FieldContainer>
+          <InputLabel htmlFor="faleMaisPlanInput">Plano (FaleMais)</InputLabel>
+          <FaleMaisSelect value={faleMaisPlan} onChange={(event) => {setFaleMaisPlan(event.target.value as FaleMaisPlan);}} id="faleMaisPlanInput">
+            <FaleMaisOption value="FaleMais30">FaleMais30</FaleMaisOption>
+            <FaleMaisOption value="FaleMais60">FaleMais60</FaleMaisOption>
+            <FaleMaisOption value="FaleMais120">FaleMais120</FaleMaisOption>
+          </FaleMaisSelect>
+        </FieldContainer>
+
+        <CalculateButton onClick={handleCalculate}>Calcular</CalculateButton>
+
+        <DisplayContainer>
+          <FieldContainer>
+            <DisplayLabel>Com FaleMais</DisplayLabel>
+            <PriceDisplay>{faleMaisPrice?.setLocale("pt-BR").toFormat() ?? "Não Disponível"}</PriceDisplay>
+          </FieldContainer>
+
+          <FieldContainer>
+            <DisplayLabel>Sem FaleMais</DisplayLabel>
+            <PriceDisplay>{defaultPrice?.setLocale("pt-BR").toFormat() ?? "Não Disponível"}</PriceDisplay>
+          </FieldContainer>
+        </DisplayContainer>
+
+      </PriceCalculator>
+    </Main>
   );
 }
 
---
+++ src/Theming/Theme.ts
@@ -0,0 +1,29 @@
+export default {
+  breakpoints: {
+    xs: "(max-width: 600px)",
+    sm: "(max-width: 960px)",
+    md: "(max-width: 1280px)",
+    lg: "(min-width: 1281px)"
+  },
+
+  color: {
+    primary: {
+      lighter: "#AFF1D5",
+      light: "#7CDEB4",
+      main: "#54C997",
+      dark: "#35B17C",
+      darker: "#19A066"
+    },
+    secondary: {
+      lighter: "#8F9BDE",
+      light: "#6373C8",
+      main: "#4355B4",
+      dark: "#273CA8",
+      darker: "#162785"
+    },
+    font: {
+      default: "#333",
+      darkContrast: "#FFF"
+    }
+  }
+};
