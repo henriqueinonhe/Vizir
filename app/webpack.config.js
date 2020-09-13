410421f74c707c6fb67dce372ccfa60ad701f6e0
Henrique Inonhe <henriqueinonhe@gmail.com>
2020-09-13 11:40:51 -0300

    Implemented production build

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,14 +1,19 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
-const frontend = {
-  mode: "development",
-  entry: "./src/App.tsx",
-  output: {
+const config = type => env => ({
+  mode: env.NODE_ENV,
+  entry: type === "frontend" ? "./src/App.tsx" : "./src/index.ts",
+  output: type === "frontend" ? {
+    path: path.resolve(__dirname, "./public"),
+    filename: "bundle.js"
+  } : 
+  {
     path: path.resolve(__dirname, "./public"),
     filename: "bundle.js"
   },
+  target: type === "frontend" ? "web" : "node",
+  externals: type === "frontend" ? [] : [nodeExternals()],
   module: {
     rules: [
       {
@@ -55,63 +60,6 @@ const frontend = {
     extensions: [".ts", ".js", ".json", ".tsx"],
     symlinks: false
   }
-};
+});
 
-const backend = {
-  mode: "development",
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "."),
-    filename: "index.js"
-  },
-  target: "node",
-  externals: [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: "ts-loader",
-          options: {
-            // transpileOnly: true
-          }
-        },
-        include: /src/
-      },
-      {
-        test: /\.m?js$/,
-        include: path.resolve(__dirname, "./src"),
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-react"
-            ],
-            plugins: [
-              "babel-plugin-styled-components"
-            ],
-            cacheDirectory: true
-          }
-        }
-      },
-      {
-        test: /\.(png|jpe?g|gif|mp3)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "assets"
-            }
-          }
-        ]
-      }
-    ]
-  },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".js", ".json", ".tsx"],
-    symlinks: false
-  }
-};
-
-module.exports = [frontend, backend];
+module.exports = [config("frontend"), config("backend")];
